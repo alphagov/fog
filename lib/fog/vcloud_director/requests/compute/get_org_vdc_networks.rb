87c062bb05fda6fb388896e329ed0bc6b97bc12b
Mike Pountney <Mike.Pountney@gmail.com>
2013-12-02 18:32:53 +0000

    add basic Mock support for get_org_vdc_networks

--- lib/fog/vcloud_director/requests/compute/get_org_vdc_networks.rb
+++ lib/fog/vcloud_director/requests/compute/get_org_vdc_networks.rb
@@ -35,6 +35,38 @@ module Fog
               "No access to entity \"(com.vmware.vcloud.entity.vdc:#{vdc_id})\"."
             )
           end
+
+          body = {
+            :OrgVdcNetworkRecord => [],
+          }
+
+          # need to handle finding shared networks for this vDC too
+          data[:networks].each do |id,attrs|
+            if data[:networks][id][:vdc] == vdc_id || data[:networks][id][:isShared]
+              body[:OrgVdcNetworkRecord] << 
+                {:name=>attrs[:name],
+                 :href=>make_href("network/#{id}"),
+                 :vdc=>make_href("vdc/#{attrs[:vdc]}"),
+                 :vdcName=>data[:vdcs][attrs[:vdc]][:name],
+                 #:connectedTo=>TODO,
+                 #linkType=>TODO,
+                 #isBusy=>TODO,
+                 :isShared=>attrs[:isShared],
+                 :gateway=>attrs[:Gateway],
+                 :netmask=>attrs[:Netmask],
+                 :dns1=>attrs[:Dns1],
+                 :dns2=>attrs[:Dns2],
+                 :dns_suffix=>attrs[:DnsSuffix],
+                }
+            end
+          end
+
+          Excon::Response.new(
+            :status => 200,
+            :headers => {'Content-Type' => "#{body[:type]};version=#{api_version}"},
+            :body => body
+          )
+
         end
 
       end
