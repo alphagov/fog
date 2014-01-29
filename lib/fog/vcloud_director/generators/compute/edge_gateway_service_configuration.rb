990892a359fef702cbd80e4969b3db111f69d677
Mike Pountney <Mike.Pountney@gmail.com>
2014-01-29 08:25:09 +0000

    [vcloud_director] Fix potential NilClass exceptions
    ... in EdgeGatewayServiceConfiguration
    
    these are very irritating to debug, and this way our broken input is passed to vCloud (for meaningful errors) rather than failing within the generator

--- lib/fog/vcloud_director/generators/compute/edge_gateway_service_configuration.rb
+++ lib/fog/vcloud_director/generators/compute/edge_gateway_service_configuration.rb
@@ -3,6 +3,7 @@ module Fog
     module Compute
       module VcloudDirector
         class EdgeGatewayServiceConfiguration
+
           def initialize(configuration={})
             @configuration = configuration
           end
@@ -27,66 +28,90 @@ module Fog
               xml.IsEnabled lb_config[:IsEnabled] if lb_config.key?(:IsEnabled)
               lb_config[:Pool].each do |pool|
                 xml.Pool {
+
                   xml.Name pool[:Name]
                   xml.Description pool[:Description] if pool.key?(:Description)
-                  pool[:ServicePort].each do |service_port|
-                    xml.ServicePort {
-                      xml.IsEnabled service_port[:IsEnabled]
-                      xml.Protocol service_port[:Protocol]
-                      xml.Algorithm service_port[:Algorithm]
-                      xml.Port service_port[:Port]
-                      xml.HealthCheckPort service_port[:HealthCheckPort]
-                      xml.HealthCheck {
-                        xml.Mode service_port[:HealthCheck][:Mode]
-                        xml.Uri service_port[:HealthCheck][:Uri]
-                        xml.HealthThreshold service_port[:HealthCheck][:HealthThreshold]
-                        xml.UnhealthThreshold service_port[:HealthCheck][:UnhealthThreshold]
-                        xml.Interval service_port[:HealthCheck][:Interval]
-                        xml.Timeout service_port[:HealthCheck][:Timeout]
+
+                  if pool.key?(:ServicePort)
+                    pool[:ServicePort].each do |service_port|
+                      xml.ServicePort {
+                        xml.IsEnabled service_port[:IsEnabled]
+                        xml.Protocol service_port[:Protocol]
+                        xml.Algorithm service_port[:Algorithm]
+                        xml.Port service_port[:Port]
+                        xml.HealthCheckPort service_port[:HealthCheckPort]
+                        if sp_healthcheck = service_port[:HealthCheck]
+                          xml.HealthCheck {
+                            xml.Mode sp_healthcheck[:Mode]
+                            xml.Uri sp_healthcheck[:Uri]
+                            xml.HealthThreshold sp_healthcheck[:HealthThreshold]
+                            xml.UnhealthThreshold sp_healthcheck[:UnhealthThreshold]
+                            xml.Interval sp_healthcheck[:Interval]
+                            xml.Timeout sp_healthcheck[:Timeout]
+                          }
+                        end
                       }
-                    }
+                    end
                   end
-                  pool[:Member].each do |member|
-                    xml.Member {
-                      xml.IpAddress member[:IpAddress]
-                      xml.Weight member[:Weight]
-                      member[:ServicePort].each do |member_service_port|
-                        xml.ServicePort {
-                          xml.Protocol member_service_port[:Protocol]
-                          xml.Port member_service_port[:Port]
-                          xml.HealthCheckPort member_service_port[:HealthCheckPort]
-                        }
-                      end
-                    }
+
+                  if pool.key?(:Member)
+                    pool[:Member].each do |member|
+                      xml.Member {
+                        xml.IpAddress member[:IpAddress]
+                        xml.Weight member[:Weight]
+                        if member.key?(:ServicePort)
+                          member[:ServicePort].each do |member_service_port|
+                            xml.ServicePort {
+                              xml.Protocol member_service_port[:Protocol]
+                              xml.Port member_service_port[:Port]
+                              xml.HealthCheckPort member_service_port[:HealthCheckPort]
+                            }
+                          end
+                        end
+                      }
+                    end
                   end
 
                 }
               end
-              lb_config[:VirtualServer].each do |virtual_server|
-                xml.VirtualServer {
-                  xml.IsEnabled virtual_server[:IsEnabled]
-                  xml.Name virtual_server[:Name]
-                  xml.Description virtual_server[:Description]
-                  xml.Interface(:href => virtual_server[:Interface][:href], :name => virtual_server[:Interface][:name])
-                  xml.IpAddress virtual_server[:IpAddress]
-                  virtual_server[:ServiceProfile].each do |service_profile|
-                    xml.ServiceProfile {
-                      xml.IsEnabled service_profile[:IsEnabled]
-                      xml.Protocol service_profile[:Protocol]
-                      xml.Port service_profile[:Port]
-                      xml.Persistence {
-                        xml.Method service_profile[:Persistence][:method]
-                        if service_profile[:Persistence][:Method] == 'COOKIE'
-                          xml.CookieName service_profile[:Persistence][:CookieName]
-                          xml.CookieMode service_profile[:Persistence][:CookieMode]
-                        end
-                      }
-                    }
-                  end
-                  xml.Logging virtual_server[:Logging]
-                  xml.Pool virtual_server[:Pool]
-                }
+
+              if lb_config.key?(:VirtualServer)
+                lb_config[:VirtualServer].each do |virtual_server|
+                  xml.VirtualServer {
+                    xml.IsEnabled virtual_server[:IsEnabled]
+                    xml.Name virtual_server[:Name]
+                    xml.Description virtual_server[:Description]
+                    if virtual_server.key?(:Interface)
+                      xml.Interface(
+                        :href => virtual_server[:Interface][:href],
+                        :name => virtual_server[:Interface][:name],
+                      )
+                    end
+                    xml.IpAddress virtual_server[:IpAddress]
+                    if virtual_server.key?(:ServiceProfile)
+                      virtual_server[:ServiceProfile].each do |service_profile|
+                        xml.ServiceProfile {
+                          xml.IsEnabled service_profile[:IsEnabled]
+                          xml.Protocol service_profile[:Protocol]
+                          xml.Port service_profile[:Port]
+                          if sp_persistence = service_profile[:Persistence]
+                            xml.Persistence {
+                              xml.Method sp_persistence[:method]
+                              if sp_persistence[:Method] == 'COOKIE'
+                                xml.CookieName sp_persistence[:CookieName]
+                                xml.CookieMode sp_persistence[:CookieMode]
+                              end
+                            }
+                          end
+                        }
+                      end
+                    end
+                    xml.Logging virtual_server[:Logging]
+                    xml.Pool virtual_server[:Pool]
+                  }
+                end
               end
+
             }
           end
 
@@ -97,22 +122,31 @@ module Fog
             xml.NatService {
               xml.IsEnabled nat_config[:IsEnabled]
 
-              nat_config[:NatRule].each do |rule|
-                xml.NatRule {
-                  xml.RuleType rule[:RuleType]
-                  xml.IsEnabled rule[:IsEnabled]
-                  xml.Id rule[:Id]
-                  gateway_nat_rule = rule[:GatewayNatRule]
-                  xml.GatewayNatRule {
-                    xml.Interface(:name => gateway_nat_rule[:Interface][:name], :href => gateway_nat_rule[:Interface][:href])
-                    xml.OriginalIp gateway_nat_rule[:OriginalIp]
-                    xml.OriginalPort gateway_nat_rule[:OriginalPort] if gateway_nat_rule.key?(:OriginalPort)
-                    xml.TranslatedIp gateway_nat_rule[:TranslatedIp]
-                    xml.TranslatedPort gateway_nat_rule[:TranslatedPort] if gateway_nat_rule.key?(:TranslatedPort)
-                    xml.Protocol gateway_nat_rule[:Protocol] if rule[:RuleType] == "DNAT"
+              if nat_config.key?(:NatRule)
+                nat_config[:NatRule].each do |rule|
+                  xml.NatRule {
+                    xml.RuleType rule[:RuleType]
+                    xml.IsEnabled rule[:IsEnabled]
+                    xml.Id rule[:Id]
+                    if gateway_nat_rule = rule[:GatewayNatRule]
+                      xml.GatewayNatRule {
+                        if gateway_nat_rule.key?(:Interface)
+                          xml.Interface(
+                            :name => gateway_nat_rule[:Interface][:name],
+                            :href => gateway_nat_rule[:Interface][:href],
+                          )
+                        end
+                        xml.OriginalIp gateway_nat_rule[:OriginalIp]
+                        xml.OriginalPort gateway_nat_rule[:OriginalPort] if gateway_nat_rule.key?(:OriginalPort)
+                        xml.TranslatedIp gateway_nat_rule[:TranslatedIp]
+                        xml.TranslatedPort gateway_nat_rule[:TranslatedPort] if gateway_nat_rule.key?(:TranslatedPort)
+                        xml.Protocol gateway_nat_rule[:Protocol] if rule[:RuleType] == "DNAT"
+                      }
+                    end
                   }
-                }
+                end
               end
+
             }
           end
 
@@ -124,31 +158,35 @@ module Fog
               xml.IsEnabled firewall_config[:IsEnabled]
               xml.DefaultAction firewall_config[:DefaultAction] if firewall_config.key?(:DefaultAction)
               xml.LogDefaultAction firewall_config[:LogDefaultAction] if firewall_config.key?(:LogDefaultAction)
-              firewall_config[:FirewallRule].each do |rule|
-                xml.FirewallRule {
-                  xml.Id rule[:Id]
-                  xml.IsEnabled rule[:IsEnabled] if rule.key?(:IsEnabled)
-                  xml.MatchOnTranslate rule[:MatchOnTranslate] if rule.key?(:MatchOnTranslate)
-                  xml.Description rule[:Description] if rule.key?(:Description)
-                  xml.Policy rule[:Policy] if rule.key?(:Policy)
 
-                  xml.Protocols {
-                    rule[:Protocols].each do |key, value|
-                      xml.send(key.to_s.capitalize, value)
+              if firewall_config.key?(:FirewallRule)
+                firewall_config[:FirewallRule].each do |rule|
+                  xml.FirewallRule {
+                    xml.Id rule[:Id]
+                    xml.IsEnabled rule[:IsEnabled] if rule.key?(:IsEnabled)
+                    xml.MatchOnTranslate rule[:MatchOnTranslate] if rule.key?(:MatchOnTranslate)
+                    xml.Description rule[:Description] if rule.key?(:Description)
+                    xml.Policy rule[:Policy] if rule.key?(:Policy)
+                    if rule.key?(:Protocols)
+                      xml.Protocols {
+                        rule[:Protocols].each do |key, value|
+                          xml.send(key.to_s.capitalize, value)
+                        end
+                      }
                     end
+                    xml.IcmpSubType rule[:IcmpSubType] if rule.key?(:IcmpSubType)
+                    xml.Port rule[:Port] if rule.key?(:Port)
+                    xml.DestinationPortRange rule[:DestinationPortRange]
+                    xml.DestinationIp rule[:DestinationIp]
+                    xml.SourcePort rule[:SourcePort] if rule.key?(:SourcePort)
+                    xml.SourcePortRange rule[:SourcePortRange]
+                    xml.SourceIp rule[:SourceIp]
+                    xml.Direction rule[:Direction] if rule.key?(:Direction) #Firewall rule direction is allowed only in backward compatibility mode.
+                    xml.EnableLogging rule[:EnableLogging] if rule.key?(:EnableLogging)
                   }
-                  xml.IcmpSubType rule[:IcmpSubType] if rule.key?(:IcmpSubType)
-                  xml.Port rule[:Port] if rule.key?(:Port)
-                  xml.DestinationPortRange rule[:DestinationPortRange]
-                  xml.DestinationIp rule[:DestinationIp]
-                  xml.SourcePort rule[:SourcePort] if rule.key?(:SourcePort)
-                  xml.SourcePortRange rule[:SourcePortRange]
-                  xml.SourceIp rule[:SourceIp]
-                  xml.Direction rule[:Direction] if rule.key?(:Direction) #Firewall rule direction is allowed only in backward compatibility mode.
-                  xml.EnableLogging rule[:EnableLogging] if rule.key?(:EnableLogging)
-                }
-
+                end
               end
+
             }
           end
 
