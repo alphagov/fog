93dd898f49e42ef94c7225a4ae983a638d36cc40
Anna Shipman <anna@annashipman.co.uk>
2014-07-02 21:09:00 +0100

    Add Mock for get_vm_disks
    The minimum information we need for our testing. Can be extended later, if required.

--- lib/fog/vcloud_director/requests/compute/get_vm_disks.rb
+++ lib/fog/vcloud_director/requests/compute/get_vm_disks.rb
@@ -26,6 +26,38 @@ module Fog
           )
         end
       end
+
+      class Mock
+
+        def get_vm_disks(id)
+          type = 'application/vnd.vmware.vcloud.rasditemslist+xml'
+
+          unless vm = data[:vms][id]
+            raise Fog::Compute::VcloudDirector::Forbidden.new(
+              'This operation is denied.'
+            )
+          end
+
+          Excon::Response.new(
+            :status => 200,
+            :headers => {'Content-Type' => "#{type};version=#{api_version}"},
+            :body => get_disks_body(id)
+          )
+        end
+
+        def get_disks_body(id)
+          {:disks=>
+            [{:address_on_parent=>0,
+              :description=>"Hard disk",
+              :name=>"Hard disk 1",
+              :id=>1001,
+              :resource_type=>17,
+              }]
+          }
+        end
+
+      end
+
     end
   end
 end
